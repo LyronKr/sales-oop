64d5cdee2b9f61a8a7e1af4cdc8b50ca0ad103a5
lyron08 <49680499+lyron08@users.noreply.github.com>
2024-11-11 09:47:25 +0800

    Database Change

--- classes/Database.php
+++ classes/Database.php
@@ -7,11 +7,5 @@
         private $db_name = "sales_oop";
         protected $conn;
 
-        public function __construct(){
-            $this->conn = new mysqli($this->server_name, $this->db_username, $this->db_password, $this->db_name);
-
-            if($this->conn->connect_error){
-                die("Unable to connect to database " . $this->db_name . " : ". $this->conn->connect_error);
-            }
-        }
+        
     }
